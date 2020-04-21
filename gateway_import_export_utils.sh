ff3b19579bb2e61282e88e7253c67c0e2a707122
MKI <mki@softwareag.com>
2020-04-21 13:19:37 +0530

    Changes are done

--- gateway_import_export_utils.sh
+++ gateway_import_export_utils.sh
@@ -14,11 +14,11 @@ shldDoImport=
 usage(){
 echo "Usage: $0a rgs"
 echo "args:"
-echo "import(or)export  To import or export from the flat file"
-echo "--api.name		The API project to import"
-echo "--apigateway.url  APIGateway url to import or export from"
-echo "--username        The APIGateway username.Default is Administrator."
-echo "--password        The APIGateway password.Default is password."
+echo "--import(or)--export  *To import or export from the flat file"
+echo "--api.name		    *The API project to import"
+echo "--apigateway.url      APIGateway url to import or export from.Default is http://localhost:5555"
+echo "--username            The APIGateway username.Default is Administrator."
+echo "--password            The APIGateway password.Default is password."
 exit
 }
 
@@ -47,6 +47,9 @@ parseArgs(){
 	  --import)
         shldDoImport='true'
       ;;
+	  --export)
+        shldDoImport='false'
+      ;;
 	  -h|--help)
         usage
 	  ;;
@@ -62,13 +65,25 @@ parseArgs(){
 main(){
 #Parseinputarguments
 parseArgs "$@"
+if [ -z "$api" ] 
+then 
+echo "API name is missing" 
+usage
+fi
+if [ -z "$shldDoImport" ] 
+then 
+echo "Missing what operation to do" 
+usage
+fi
+
 if [ "$shldDoImport" = "true" ]
 then
     echo "Importing the API"
 	if [ -d "$CURR_DIR/apis/$api" ] 
 	then
-	zip $api.zip $CURR_DIR/apis/$api/*
-	curl -i -X POST $url/rest/apigateway/archive?overwrite=* -H "Content-Type:application/zip" -H"Accept:application/json" --data-binary @"$CURR_DIR/$api.zip" -u$username:$password
+	cd $CURR_DIR/apis/$api/ && zip -r $CURR_DIR/$api.zip ./*
+	curl -i -X POST $url/rest/apigateway/archive?overwrite=* -H "Content-Type:application/zip" -H"Accept:application/json" --data-binary @"$CURR_DIR/$api.zip" -u $username:$password
+	rm $CURR_DIR/$api.zip
 	else
 	echo "The API with name $api does not exists in the flat file."
 	fi
@@ -76,7 +91,8 @@ else
 	if [ -d "$CURR_DIR/apis/$api" ] 
 	then
 	curl $url/rest/apigateway/archive -d @"$CURR_DIR/apis/$api/export_payload.json" --output $CURR_DIR/$api.zip -u $username:$password -H "x-HTTP-Method-Override:GET" -H "Content-Type:application/json"
-	unzip $CURR_DIR/$api.zip $CURR_DIR/apis/$api/
+	unzip -o $CURR_DIR/$api.zip -d $CURR_DIR/apis/$api/
+	rm $CURR_DIR/$api.zip
 	else
 	echo "The API with name $api does not exists in the flat file."
 	fi
